69d55405a5d0162ef065c2712e0906e6c857144a
DarkcrisKQ <cdiogo1235@gmail.com>
2023-05-01 12:24:16 -0300

    Add files via upload

---
+++ Numero secreto/app/Validação.js
@@ -0,0 +1,27 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+    const numero = +chute
+
+    if (chuteForInvalido(numero)) {
+            if (chute.toUpperCase() === "GAME OVER") {
+    
+                document.body.innerHTML =
+                    `
+                    <h2>Game Over!!!</h2>
+                    <h3>Pressione o botão para jogar novamente</h3>
+                    <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                    `
+                    document.body.style.backgroundColor = "black";
+            } else {
+    
+                elementoChute.innerHTML += '<div>Valor Inválido</div>';
+            }
+        }
+    
+    }
+    
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero){
+    return numero > maiorValor || numero < menorValor;
+}
+
+
